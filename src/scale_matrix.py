b64acf3b154c12895c6ed9142d455d3de2a1e1fe
jaemin6 <gumt555@gmail.com>
2025-07-31 10:17:49 +0900

    보간법 적용 이미지 확대, 축소 코드 작성

--- src/scale_matrix.py
+++ src/scale_matrix.py
@@ -4,13 +4,22 @@ import numpy as np
 img = cv2.imread('../img/fish.jpg')
 height, width = img.shape[:2]
 
+# 0.5배 축소 변환 행렬
+m_small = np.float32([[0.5, 0, 0],
+                       [0, 0.5,0]])  
+# 2배 확대 변환 행렬
+m_big = np.float32([[2, 0, 0],
+                     [0, 2, 0]])  
 
-m_small = np.float32([[0.5, 0, 0], [0, 0.5, 0]])
-m_big = np.float32([[2, 0, 0], [0, 2, 0]])
+# 보간법 적용 없이 확대 축소
+dst1 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)))
+dst2 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)))
 
-dst_small = cv2.warpAffine(img, m_small, (int(w*0.5), int(h*0.5)))
-dst_big   = cv2.warpAffine(img, m_big,   (int(w*2),   int(h*2))
-
+# 보간법 적용한 확대 축소
+dst3 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)), \
+                        None, cv2.INTER_AREA)
+dst4 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)), \
+                        None, cv2.INTER_CUBIC)
 
 # 결과 출력
 cv2.imshow("original", img)
