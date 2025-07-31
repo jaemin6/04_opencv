c20020db2ece07d621e87a6a23809172c4043eff
jaemin6 <gumt555@gmail.com>
2025-07-31 10:31:18 +0900

    rotate 코드 작성

--- src/rotate_martix.py
+++ src/rotate_martix.py
@@ -2,14 +2,25 @@ import cv2
 import numpy as np
 
 img = cv2.imread('../img/fish.jpg')
-rows, cols = img.shape[:2]
+rows,cols = img.shape[0:2]
 
-angle = 45 * np.pi / 180  # 회전 각도 
-m = np.float32([[np.cos(angle), -np.sin(angle), tx],
-                [np.sin(angle),  np.cos(angle), ty]])
+# 라디안 각도 계산(60진법을 호도법으로 변경)
+d45 = 45.0 * np.pi / 180    # 45도
+d90 = 90.0 * np.pi / 180    # 90도
 
-rotated = cv2.warpAffine(img, m, (cols, rows))
+# 회전을 위한 변환 행렬 생성
+m45 = np.float32( [[ np.cos(d45), -1* np.sin(d45), rows//2],
+                    [np.sin(d45), np.cos(d45), -1*cols//4]])
+m90 = np.float32( [[ np.cos(d90), -1* np.sin(d90), rows],
+                    [np.sin(d90), np.cos(d90), 0]])
 
-cv2.imshow("rotated", rotated)
+# 회전 변환 행렬 적용
+r45 = cv2.warpAffine(img,m45,(cols,rows))
+r90 = cv2.warpAffine(img,m90,(rows,cols))
+
+# 결과 출력
+cv2.imshow("origin", img)
+cv2.imshow("45", r45)
+cv2.imshow("90", r90)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
